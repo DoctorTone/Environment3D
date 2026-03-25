93a97fab8786254316ce74f8dbb6363aa6c0ec4a
DoctorTone <tony@drt-software.com>
2026-03-25 00:16:01 +0000

    Update cam position and target, update ground plane colour

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <Canvas
-        camera={{ position: [0, 1200, 1500], fov: 50, near: 10, far: 10000 }}
+        camera={{ position: [2000, 1200, 3000], fov: 50, near: 10, far: 10000 }}
         gl={{ antialias: true }}
       >
         {/* <ResponsiveCamera /> */}
@@ -25,7 +25,7 @@ function App() {
           enableDamping={true}
           minPolarAngle={0}
           maxPolarAngle={Math.PI / 2}
-          target={[TARGET_POSITION.X, TARGET_POSITION.Y, TARGET_POSITION.Z]}
+          target={[2000, 0, 500]}
           minDistance={500}
           maxDistance={10000}
         />
--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -1,8 +1,12 @@
 export default function Ground() {
   return (
-    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]} receiveShadow>
-      <planeGeometry args={[8000, 4000]} />
-      <meshStandardMaterial color="#a39dbb" />
+    <mesh
+      rotation={[-Math.PI / 2, 0, 0]}
+      position={[2000, 0, 500]}
+      receiveShadow
+    >
+      <planeGeometry args={[8000, 6000]} />
+      <meshStandardMaterial color="#0a0a0a" />
     </mesh>
   );
 }
