8277348f29a32eb9b411224d813b4cd763278aac
DoctorTone <tony@drt-software.com>
2026-03-25 00:18:51 +0000

    Update ground plane size and position

--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -2,10 +2,10 @@ export default function Ground() {
   return (
     <mesh
       rotation={[-Math.PI / 2, 0, 0]}
-      position={[2000, 0, 500]}
+      position={[1900, 0, 750]}
       receiveShadow
     >
-      <planeGeometry args={[8000, 6000]} />
+      <planeGeometry args={[7500, 5000]} />
       <meshStandardMaterial color="#0a0a0a" />
     </mesh>
   );
